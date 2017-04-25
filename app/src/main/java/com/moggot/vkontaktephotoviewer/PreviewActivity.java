f3a320650ffd2eb606ad322edc4e75a357022272
moggot <9.tilov@gmail.com>
2017-04-25 18:16:11 +0300

    добавлено приложение

---
+++ app/src/main/java/com/moggot/vkontaktephotoviewer/PreviewActivity.java
@@ -0,0 +1,17 @@
+package com.moggot.vkontaktephotoviewer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by toor on 25.04.17.
+ */
+
+public class PreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.preview_activity);
+    }
+}
